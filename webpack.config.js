1c99f646ccedb17570f62e8f8e44b7cb26c70459
Seth Burkart <SethBurkart7@gmail.com>
2023-08-13 14:30:21 +1000

    add webpack to organize code

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'SEQTA': './src/SEQTA.js',
+    'background': './src/background.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        include: path.resolve(__dirname, 'src'),
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'src/[path][name][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public', to: '.' }, // Copies everything from the public folder to the root level of build
+        { from: 'src/inject', to: 'inject' },
+      ],
+    }),
+  ],
+};
