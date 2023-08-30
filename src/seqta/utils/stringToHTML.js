6b5a843410d516698814d893a39e74127bc0d5f8
SethBurkart123 <hi25S.burkart@hills.adventist.edu.au>
2023-08-31 06:08:10 +1000

    improve styling, fix dark mode bugs

--- src/seqta/utils/stringToHTML.js
+++ src/seqta/utils/stringToHTML.js
@@ -1,6 +1,6 @@
 import DOMPurify from "dompurify";
 
-export function stringToHTML(str, styles = false) {
+export default function stringToHTML(str, styles = false) {
   var parser = new DOMParser();
   str = DOMPurify.sanitize(str, { ADD_ATTR: ["onclick"] });
   var doc = parser.parseFromString(str, "text/html");
