73ce0a0b8a5214cc29d1dab1d0194636c57bf2a3
SethBurkart123 <hi25S.burkart@hills.adventist.edu.au>
2023-09-05 11:57:59 +1000

    ui fixes

--- src/SEQTA.js
+++ src/SEQTA.js
@@ -1874,7 +1874,7 @@ function AddBetterSEQTAElements(toggle) {
         titlebar.classList.add("titlebar");
         let container = document.getElementById("content");
         container.append(titlebar);
-        var NewButtonStr = "<li class=\"item\" data-key=\"home\" id=\"homebutton\" data-path=\"/home\" data-betterseqta=\"true\"><label><svg style=\"width:24px;height:24px\" viewBox=\"0 0 24 24\"><path fill=\"currentColor\" d=\"M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z\" /></svg>Home</label></li>";
+        var NewButtonStr = "<li class=\"item\" data-key=\"home\" id=\"homebutton\" data-path=\"/home\" data-betterseqta=\"true\"><label><svg style=\"width:24px;height:24px\" viewBox=\"0 0 24 24\"><path fill=\"currentColor\" d=\"M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z\" /></svg><span>Home</span></label></li>";
         var NewButton = stringToHTML(NewButtonStr);
         var menu = document.getElementById("menu");
         var List = menu.firstChild;
