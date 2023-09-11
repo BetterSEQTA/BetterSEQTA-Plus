1e1c5f9b408c5f3c1900cd9cfba96c4dc4220411
SethBurkart123 <hi25S.burkart@hills.adventist.edu.au>
2023-09-12 07:06:56 +1000

    removed safari from script

--- package.sh
+++ package.sh
@@ -1,7 +1,3 @@
 echo "Packaging the application from build/"
 7z a -tzip package.zip ./build/*
 echo "Done. Package is in package.zip and is ready to publish!"
-
-# Updating safari Xcode Project
-echo "Packaging for Safari"
-cp ./build/* "./safari/Shared (Extension)\Resources"
