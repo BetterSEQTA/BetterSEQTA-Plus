f8c84494ba98c3f33ed2346516d7ab8dbd776517
Seth Burkart <sethburkart@Seths-MacBook-Pro.local>
2023-09-15 12:08:33 +1000

    add about page

--- interface/src/pages/About.tsx
+++ interface/src/pages/About.tsx
@@ -1,8 +1,18 @@
 const About: React.FC = () => {
 
   return (
-    <div className="flex flex-col overflow-y-scroll divide-y divide-zinc-100">
-      <h2>About</h2>
+    <div className="flex flex-col overflow-y-scroll divide-y divide-zinc-100/50">
+      <div>
+        <h2 className="text-lg font-bold">About</h2>
+        <p className="py-2">BetterSEQTA+ is a branch of BetterSEQTA which was originally developed by Nulkem. It was discontinued. So BetterSEQTA+ has come in to fill in that gap!</p>
+        <p className="py-2">We are currently working on fixing bugs and adding new features. If you want to request a feature or report a bug, you can do so on 
+          <a className="pl-1 text-blue-500 underline hover:text-blue-600" href="https://github.com/SethBurkart123/EvenBetterSEQTA" target="_blank">Github</a>.
+        </p>
+      </div>
+      <div>
+        <h2 className="pt-2 text-lg font-bold">Credits</h2>
+        <p className="py-2">Nulkem for the original extension, OG-RandomTechChannel, Crazypersonalph, and the current maintainer SethBurkart123</p>
+      </div>
     </div>
   );
 };
