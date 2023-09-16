d44271335694c7ef9f0fa0d29e2a7d2765515eb9
SethBurkart123 <hi25S.burkart@hills.adventist.edu.au>
2023-09-16 20:39:09 +1000

    improvements

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  important: true,
+  darkMode: "class",
+  theme: {
+    fontSize: {
+      "xs": ".65rem",
+      "sm": ".775rem",
+      "base": "0.65rem",
+      "md": "0.65rem",
+      "lg": "1rem",
+      "xl": "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+    }
+  },
+  plugins: [],
+};
