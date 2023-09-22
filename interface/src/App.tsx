f5cc56c9d918e80d19ef0f69f2976c196375043e
SethBurkart123 <hi25S.burkart@hills.adventist.edu.au>
2023-09-22 13:49:52 +1000

    add dark mode to popup

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -37,7 +37,6 @@ const App: React.FC = () => {
     }
   ];
 
-  {/* <div className="flex justify-center w-screen h-screen pt-4 overflow-hidden" style={{ background: settingsState.customThemeColor }}> */}
   return (
     <div className="flex flex-col w-[384px] shadow-2xl gap-2 bg-white rounded-xl h-[590px] dark:bg-zinc-800 dark:text-white">
       <div className="grid border-b border-b-zinc-200/40 place-items-center">
