e0b4a2963519d4c96e1eac78a6a4cba85384ba32
SethBurkart123 <hi25S.burkart@hills.adventist.edu.au>
2023-10-20 06:37:52 +1100

    added multiple custom backgrounds

--- interface/tailwind.config.js
+++ interface/tailwind.config.js
@@ -15,6 +15,22 @@ export default {
       "xl": "1.25rem",
       "2xl": "1.5rem",
       "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem",
+      "7xl": "5rem",
+      "8xl": "6rem",
+      "9xl": "8rem",
+      "10xl": "10rem",
+      "11xl": "12rem",
+      "12xl": "14rem",
+      "13xl": "16rem",
+      "14xl": "18rem",
+    },
+    extend: {
+      fontFamily: {
+        "IconFamily": "IconFamily"
+      }
     }
   },
   plugins: [],
