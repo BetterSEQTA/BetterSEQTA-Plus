1819a82350866bdd6d1da272c122d5ff0b7d8e33
SethBurkart123 <hi25S.burkart@hills.adventist.edu.au>
2023-11-02 17:32:38 +1100

    themeselector new design

--- interface/src/assets/themes.tsx
+++ interface/src/assets/themes.tsx
@@ -1,8 +1,10 @@
+import test from './929098bd-40b9-46a5-bd6f-8b9e7d5e648b.webp';
+
 const themes = [
   {
     name: "Dark",
     url: "https://raw.githubusercontent.com/SethBurkart123/BetterSEQTA-Themes/main/themes/dark.json",
-    coverImage: <img />,
+    coverImage: <img className="object-cover object-center w-full h-full" src={test} />,
   },
   {
     name: "Light",
