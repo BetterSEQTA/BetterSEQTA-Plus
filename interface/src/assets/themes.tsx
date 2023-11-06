9919b25493e47d24b449f1053bb8405c0b2977cc
SethBurkart123 <hi25S.burkart@hills.adventist.edu.au>
2023-11-06 13:31:20 +1100

    minor design imporvements

--- interface/src/assets/themes.tsx
+++ interface/src/assets/themes.tsx
@@ -1,4 +1,5 @@
-import test from './929098bd-40b9-46a5-bd6f-8b9e7d5e648b.webp';
+import test from './themeCovers/929098bd-40b9-46a5-bd6f-8b9e7d5e648b.webp';
+import hacker from './themeCovers/hacker.jpeg'
 
 const themes = [
   {
@@ -7,9 +8,9 @@ const themes = [
     coverImage: <img className="object-cover object-center w-full h-full" src={test} />,
   },
   {
-    name: "Light",
-    url: "https://raw.githubusercontent.com/SethBurkart123/BetterSEQTA-Themes/main/themes/light.json",
-    coverImage: <img />,
+    name: "Hacker",
+    url: "https://raw.githubusercontent.com/SethBurkart123/BetterSEQTA-Themes/main/themes/hacker.json",
+    coverImage: <img className="object-cover object-center w-full h-full" src={hacker} />,
   },
 ];
 
