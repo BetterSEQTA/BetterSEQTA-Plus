3c5769e27543f14cd94c4924966261a91dc8f1c0
SethBurkart123 <hi25S.burkart@hills.adventist.edu.au>
2023-11-14 12:08:18 +1100

    separate backgroundselector file

---
+++ interface/src/components/backgroundSelector/DownloadProgressCircle.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface DownloadProgressCircleProps {
+  progress: number;
+}
+
+export const DownloadProgressCircle: React.FC<DownloadProgressCircleProps> = ({ progress }) => {
+  const calcCircumference = (radius: number) => 2 * Math.PI * radius;
+
+  return (
+    <svg className="w-full h-full text-zinc-100 dark:text-zinc-700" viewBox="0 0 36 36">
+      <circle stroke="currentColor" fill="none" strokeWidth="4" strokeLinecap="round" cx="18" cy="18" r="10" strokeDasharray={`${calcCircumference(14)} ${calcCircumference(14)}`} strokeDashoffset="0" transform="rotate(-90 18 18)"></circle>
+      <circle stroke="#3B82F6" fill="none" strokeWidth="4" strokeLinecap="round" cx="18" cy="18" r="10" strokeDasharray={`${calcCircumference(14)} ${calcCircumference(14)}`} strokeDashoffset={`${calcCircumference(14) * (1 - (progress / 100))}`} transform="rotate(-90 18 18)"></circle>
+    </svg>
+  );
+};
