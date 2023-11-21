6dd75462b305e127de3b5a97fb9cffab57782186
SethBurkart123 <hi25S.burkart@hills.adventist.edu.au>
2023-11-22 09:35:06 +1100

    fixed bug when enabling a theme, reloading breaks colors

--- src/seqta/ui/Themes.js
+++ src/seqta/ui/Themes.js
@@ -1,5 +1,5 @@
+/* global chrome */
 import localforage from 'localforage';
-import { updateAllColors } from '../ui/colors/Manager.js';
 
 let currentThemeClass = '';
 
@@ -77,8 +77,7 @@ const applyTheme = async (themeName) => {
     );
   }
 
-  // Update colors based on theme settings
-  updateAllColors(darkMode, defaultColour);
+  chrome.storage.local.set({ DarkMode: darkMode, selectedColor: defaultColour });
 };
 
 export const listThemes = async () => {
