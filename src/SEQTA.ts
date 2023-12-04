2b113e20201d7c56f9a407ba469cb6c51170d500
Crazypersonalph <93847055+Crazypersonalph@users.noreply.github.com>
2023-12-04 21:47:35 +0800

    fixed name detail stuff

--- src/SEQTA.ts
+++ src/SEQTA.ts
@@ -1199,7 +1199,9 @@ async function AddBetterSEQTAElements(toggle: any) {
         }
 
         const container = document.getElementById('content')!;
-        container.append(document.createElement('div').classList.add('titlebar')!);
+        const div = document.createElement('div')
+        div.classList.add('titlebar');
+        container.append(div);
         const NewButton = stringToHTML('<li class="item" data-key="home" id="homebutton" data-path="/home" data-betterseqta="true"><label><svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z" /></svg><span>Home</span></label></li>');
 
         const menu = document.getElementById('menu')!;
@@ -1231,7 +1233,7 @@ async function AddBetterSEQTAElements(toggle: any) {
           const titlebar = document.getElementsByClassName('titlebar')[0];
           const userInfo = stringToHTML(
             '<div class="userInfosvgdiv tooltip"><svg class="userInfosvg" viewBox="0 0 24 24"><path fill="var(--text-primary)" d="M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z"></path></svg><div class="tooltiptext topmenutooltip" id="logouttooltip"></div></div>',
-          ).firstChild
+          ).firstChild;
           titlebar.append(userInfo!);
       
           const userinfo = stringToHTML(`<div class="userInfo"><div class="userInfoText"><div style="display: flex; align-items: center;"><p class="userInfohouse userInfoCode"></p><p class="userInfoName">${info.userDesc}</p></div><p class="userInfoCode">${UserInitalCode}</p></div></div>`).firstChild;
