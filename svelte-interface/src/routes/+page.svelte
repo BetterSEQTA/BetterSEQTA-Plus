335cdd7f6ee41bfe014abfe32f781e179c54cecb
SethBurkart123 <hi25S.burkart@hills.adventist.edu.au>
2023-12-18 06:08:38 +1100

    dev svelte

--- svelte-interface/src/routes/+page.svelte
+++ svelte-interface/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let test = '';
+</script>
+
+<h1>{test}</h1>
+<input bind:value={test} />
