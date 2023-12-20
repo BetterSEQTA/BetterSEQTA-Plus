6d896caebfae2a45037b2e445f1c685bce3b3d73
SethBurkart123 <hi25S.burkart@hills.adventist.edu.au>
2023-12-20 13:15:06 +1100

    move telemetry code to only run on Seqta pages

--- src/SEQTA.ts
+++ src/SEQTA.ts
@@ -23,26 +23,6 @@ import stringToHTML from './seqta/utils/stringToHTML';
 import { updateAllColors } from './seqta/ui/colors/Manager';
 import { updateBgDurations } from './seqta/ui/Animation';
 
-browser.storage.local.get([ "telemetry" ]).then((telemetry) => {
-  if (telemetry.telemetry === true) {
-    Sentry.init({
-      dsn: "https://54bdb68e80b45182ded22ecf9fe9529c@o4506347383291904.ingest.sentry.io/4506347462393856",
-      integrations: [
-        new Sentry.BrowserTracing({
-          // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-          tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-        }),
-        new Sentry.Replay(),
-      ],
-      // Performance Monitoring
-      tracesSampleRate: 1.0, // Capture 100% of the transactions
-      // Session Replay
-      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-      replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-    });
-  }
-})
-
 declare global {
   interface Window {
     chrome?: any;
@@ -708,6 +688,24 @@ function main(storedSetting: SettingsState) {
   // Handle undefined onoff setting
   if (typeof storedSetting.onoff === 'undefined') {
     browser.runtime.sendMessage({ type: 'setDefaultStorage' });
+  }
+
+  if (storedSetting.telemetry && storedSetting.onoff) {
+    Sentry.init({
+      dsn: "https://54bdb68e80b45182ded22ecf9fe9529c@o4506347383291904.ingest.sentry.io/4506347462393856",
+      integrations: [
+        new Sentry.BrowserTracing({
+          // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+          tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+        }),
+        new Sentry.Replay(),
+      ],
+      // Performance Monitoring
+      tracesSampleRate: 1.0, // Capture 100% of the transactions
+      // Session Replay
+      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+      replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    });
   }
 
   const handleDisabled = () => {
