9a53c56141e8cd91c81ec76895c54f01db57278d
Alphons <93847055+Crazypersonalph@users.noreply.github.com>
2024-02-09 19:20:33 +0800

    clean up sentry stuff, fix all of my quota being used 💀

--- src/background.ts
+++ src/background.ts
@@ -6,17 +6,10 @@ browser.storage.local.get([ "telemetry" ]).then((telemetry) => {
     Sentry.init({
       dsn: "https://54bdb68e80b45182ded22ecf9fe9529c@o4506347383291904.ingest.sentry.io/4506347462393856",
       integrations: [
-        new Sentry.BrowserTracing({
-          // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-          tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-        }),
-        new Sentry.Replay(),
+        Sentry.browserTracingIntegration()
       ],
       // Performance Monitoring
       tracesSampleRate: 1.0, // Capture 100% of the transactions
-      // Session Replay
-      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-      replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
     });
   }
 })
