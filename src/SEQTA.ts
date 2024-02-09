1330ff56829b1e01b20631e49fd92a9af9fe265e
Alphons <93847055+Crazypersonalph@users.noreply.github.com>
2024-02-09 19:20:33 +0800

    clean up sentry stuff, fix all of my quota being used 💀

--- src/SEQTA.ts
+++ src/SEQTA.ts
@@ -756,14 +756,9 @@ function main(storedSetting: SettingsState) {
   if (storedSetting.telemetry && storedSetting.onoff) {
     Sentry.init({
       dsn: "https://54bdb68e80b45182ded22ecf9fe9529c@o4506347383291904.ingest.sentry.io/4506347462393856",
-      integrations: [
-        Sentry.replayIntegration(),
-      ],
+      integrations: [Sentry.browserTracingIntegration()],
       // Performance Monitoring
       tracesSampleRate: 1.0, // Capture 100% of the transactions
-      // Session Replay
-      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-      replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
     })
   }
 
