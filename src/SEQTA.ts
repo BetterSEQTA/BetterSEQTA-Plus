2a6ad31a04130f0c15b1ec1ce71804c09053b0dd
97-42 <144976307+97-42@users.noreply.github.com>
2024-03-01 18:22:21 +0800

    Update SEQTA.ts

--- src/SEQTA.ts
+++ src/SEQTA.ts
@@ -149,7 +149,7 @@ export function OpenWhatsNewPopup() {
   textcontainer.classList.add('whatsnewTextContainer')
 
   let textheader: any = stringToHTML(
-    '<h1 class="whatsnewTextHeader">RECENT UPDATES</h1>',
+    '<h1 class="whatsnewTextHeader">WHATS NEW</h1>',
   ).firstChild
   textcontainer.append(textheader)
 
