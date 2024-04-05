c7e7b41ce9a03b0645de1e18b09e5fe1a6b0d900
SethBurkart123 <seth.burkart@hills.adventist.edu.au>
2024-04-05 13:08:47 +1100

    save selected color when enabling themes

--- src/seqta/ui/themes/setTheme.ts
+++ src/seqta/ui/themes/setTheme.ts
@@ -12,15 +12,20 @@ export const setTheme = async (themeId: string) => {
 
     console.debug('Loading theme', theme);
 
+    let originalSelectedColor = { selectedColor: '' };
+
     // Remove the currently enabled theme
     if (enabledTheme.selectedTheme) {
       const currentTheme = await localforage.getItem(enabledTheme.selectedTheme) as CustomTheme;
       if (currentTheme) {
         removeTheme(currentTheme);
       }
+      const color = await browser.storage.local.get('originalSelectedColor') as { originalSelectedColor: string; };
+      originalSelectedColor = { selectedColor: color.originalSelectedColor };
+    } else {
+      originalSelectedColor = await browser.storage.local.get('selectedColor') as { selectedColor: string; };
     }
 
-    const originalSelectedColor = await browser.storage.local.get('selectedColor') as { selectedColor: string; };
     await applyTheme(theme);
 
     await browser.storage.local.set({
