60a907a32c8eda54bd1d3595303e54a44f283fb5
Crazypersonalph <93847055+Crazypersonalph@users.noreply.github.com>
2024-05-19 22:50:00 +0800

    remove sentry due to filled quote and not much use

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { crx } from '@crxjs/vite-plugin'
 import million from "million/compiler"
 import manifest from './manifest.json'
 import react from '@vitejs/plugin-react-swc'
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { join } from 'path'
 
 export default defineConfig({
@@ -16,16 +15,6 @@ export default defineConfig({
       }      
     }),
     crx({ manifest }),
-    sentryVitePlugin({
-      org: process.env.SENTRY_ORG,
-      project: 'betterseqtaplus-main',
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-    }),
-    sentryVitePlugin({
-      org: process.env.SENTRY_ORG,
-      project: 'betterseqtaplus-popup',
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-    }),
   ],
   server: {
     port: 5173,
