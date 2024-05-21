ca4cdc870cd85de48d3bb602d50cd1a06f64cc4e
SethBurkart123 <seth.burkart@hills.adventist.edu.au>
2024-05-21 19:55:45 +1000

    fix: store no results in dark mode

--- src/interface/pages/Store.tsx
+++ src/interface/pages/Store.tsx
@@ -337,8 +337,8 @@ const Store = () => {
         </div>
         {filteredThemes.length == 0 && !loading && (
           <div className="flex flex-col items-center justify-center w-full text-center h-96">
-            <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">That doesnt exist! 😭😭😭</h1>
-            <p className="mt-6 text-lg leading-7 text-gray-600">Sorry, we couldn’t find the theme you’re looking for. Maybe... you could create it?</p>
+            <h1 className="mt-4 text-3xl font-bold tracking-tight text-zinc-900 dark:text-zinc-100 sm:text-5xl">That doesnt exist! 😭😭😭</h1>
+            <p className="mt-6 text-lg leading-7 text-zinc-600 dark:text-zinc-300">Sorry, we couldn’t find the theme you’re looking for. Maybe... you could create it?</p>
           </div>
         )}
       </div>
