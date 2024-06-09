2d225fe85d0b97b18fbb68372b19a9b4dc44f7bf
sethburkart123 <seth.burkart@hills.adventist.edu.au>
2024-06-09 14:50:53 +1000

    refactor: clean up and fix bg durations

--- src/seqta/ui/Animation.ts
+++ src/seqta/ui/Animation.ts
@@ -1,20 +1,18 @@
+import { settingsState } from "../utils/listeners/SettingsState";
+
 /**
  * Update the background animation durations based on the slider input.
  * @param {Object} item - The object containing the bksliderinput property.
  * @param {number} [minDuration=1] - The minimum animation duration in seconds.
  * @param {number} [maxDuration=10] - The maximum animation duration in seconds.
  */
-export function updateBgDurations(speed: any, minDuration = 0.5, maxDuration = 10) {
+export function updateBgDurations(minDuration: number = 0.5, maxDuration: number = 10) {
   // Class names to look for
   const bgClasses = ['bg', 'bg2', 'bg3'];
   let reversedValue: any;
 
-  if (speed.bksliderinput === undefined) {
-    // Reverse the slider direction to align with the animation
-    reversedValue = 150 - speed;
-  } else {
-    reversedValue = 150 - speed.bksliderinput;
-  }
+  // Reverse the slider direction to align with the animation
+  reversedValue = 150 - parseInt(settingsState.bksliderinput);
   
   // Range of possible animation durations
   const durationRange = maxDuration - minDuration;
