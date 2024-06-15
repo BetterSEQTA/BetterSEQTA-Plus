a1582b0e749a9e3e230345a3b58348dc013efa10
sethburkart123 <seth.burkart@hills.adventist.edu.au>
2024-06-16 08:14:51 +1000

    fix: separate firefox dev and build scripts

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,22 @@ import { defineConfig } from 'vite';
 import { join } from 'path';
 
 import manifest from './manifest.json';
+import firefoxManifest from './manifest.firefox.json';
 
 import react from '@vitejs/plugin-react-swc';
 //import MillionLint from '@million/lint';
 import { crx } from '@crxjs/vite-plugin';
 import million from "million/compiler";
 
+const isFirefox = process.env.VITE_TARGET === 'firefox';
+
 const plugins = [
   react(),
   million.vite({ auto: true }),
   //MillionLint.vite(), /* enable for testing and debugging performance */
   crx({
-  manifest
+    manifest: isFirefox ? firefoxManifest : manifest,
+    browser: isFirefox ? 'firefox' : 'chrome'
   })
 ];
 
