4a8ed32d3e9e77f4d23be374b61530f1a8d38947
Alphons Joseph <93847055+Crazypersonalph@users.noreply.github.com>
2024-09-05 20:45:52 +0800

    remove redundant code

--- src/svelte-interface/pages/settings/shortcuts.svelte
+++ src/svelte-interface/pages/settings/shortcuts.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-  
+    function formatUrl(inputUrl: string) {
+        const protocolRegex = /^(http:\/\/|https:\/\/|ftp:\/\/)/;
+        return protocolRegex.test(inputUrl) ? inputUrl : `https://${inputUrl}`;
+    }
 </script>
 
 <div class="text-xl">shortcuts tab</div>
--- src/svelte-interface/types/SwitchProps.ts
+++
@@ -1,6 +0,0 @@
-import "./Switch.css";
-
-export interface SwitchProps {
-    onChange: (isOn: boolean) => void;
-    state: boolean;
-}
