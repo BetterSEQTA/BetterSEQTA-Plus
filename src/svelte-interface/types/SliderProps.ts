e8c43d48f75d93082560c43ff7b9d6961c0cc549
Alphons Joseph <93847055+Crazypersonalph@users.noreply.github.com>
2024-09-05 20:45:52 +0800

    remove redundant code

--- src/svelte-interface/types/SliderProps.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import "./Slider.css";
-export interface Slider {
-    onValueChange: (value: number) => void;
-}
-declare const Slider: React.FC<Slider>;
-export default Slider;
