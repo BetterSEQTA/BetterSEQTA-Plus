5ec28e9fa6754938af6e5f18b1f710e0b72aa920
sethburkart123 <seth.burkart@hills.adventist.edu.au>
2024-09-13 17:36:31 +1000

    fix(browser): fixed settings interface in firefox #166

--- src/svelte-interface/components/Button.svelte
+++ src/svelte-interface/components/Button.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  let { onClick, text, ...props } = $props<{ onClick: () => void, text: string, [key: string]: any }>();
+  let { onClick, text } = $props<{ onClick: () => void, text: string, [key: string]: any }>();
 </script>
 
-<button onclick={onClick} class='px-4 py-1 text-[0.75rem] dark:bg-[#38373D] bg-[#DDDDDD] dark:text-white rounded-md' {...props}>
+<button onclick={onClick} class='px-4 py-1 text-[0.75rem] dark:bg-[#38373D] bg-[#DDDDDD] dark:text-white rounded-md'>
   {text}
 </button>
