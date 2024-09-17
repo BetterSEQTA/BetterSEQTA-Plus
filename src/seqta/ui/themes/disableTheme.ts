c9550d0d377cfe9a1b526707873e376c23340f9e
sethburkart123 <seth.burkart@hills.adventist.edu.au>
2024-09-18 09:00:40 +1000

    feat(settings_sync): add syncing with store

--- src/seqta/ui/themes/disableTheme.ts
+++ src/seqta/ui/themes/disableTheme.ts
@@ -8,11 +8,10 @@ const mutex = new Mutex();
 let isDisabling = false;
 
 export const disableTheme = async () => {
-  console.log('Disabling theme', isDisabling)
   if (isDisabling) return;
 
   if (!settingsState.selectedTheme || settingsState.selectedTheme === '') {
-    console.log('Theme is already disabled, exit early')
+    console.debug('Theme is already disabled, exit early')
     // Theme is already disabled, exit early
     return;
   }
@@ -20,7 +19,7 @@ export const disableTheme = async () => {
   const unlock = await mutex.lock();
   try {
     if (settingsState.selectedTheme) {
-      console.log('Disabling theme:', settingsState.selectedTheme);
+      console.debug('Disabling theme:', settingsState.selectedTheme);
       const theme = await localforage.getItem(settingsState.selectedTheme) as CustomTheme;
       if (theme) {
         await removeTheme(theme);
