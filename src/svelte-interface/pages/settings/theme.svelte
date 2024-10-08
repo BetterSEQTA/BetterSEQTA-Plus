1e0ac66617fdbd9535b8cfe29ee18ae5cef8418a
sethburkart123 <seth.burkart@hills.adventist.edu.au>
2024-10-08 18:50:57 +1100

    feat(ThemePage): add edit mode button

--- src/svelte-interface/pages/settings/theme.svelte
+++ src/svelte-interface/pages/settings/theme.svelte
@@ -16,6 +16,14 @@
     class="w-full px-4 py-2 mb-4 text-[13px] dark:text-white transition rounded-xl bg-zinc-200 dark:bg-zinc-700/50">
     { clearTheme ? 'Clear Theme' : 'Select a Theme' }
   </button>
-  <BackgroundSelector isEditMode={editMode} bind:selectedBackground={selectedBackground} bind:selectNoBackground={selectNoBackground} />
-  <ThemeSelector isEditMode={editMode} />
+  <div class="relative w-full">
+    <!-- edit button -->
+
+    <button
+      onclick={() => editMode = !editMode}
+      class="absolute top-0 right-0 z-10 w-8 h-8 text-lg rounded-xl font-IconFamily bg-zinc-100 dark:bg-zinc-700">{editMode ? '\ue9e4' : '\uec38'}</button>
+    <!-- edit button -->
+    <BackgroundSelector isEditMode={editMode} bind:selectedBackground={selectedBackground} bind:selectNoBackground={selectNoBackground} />
+    <ThemeSelector isEditMode={editMode} />
+  </div>
 </div>
