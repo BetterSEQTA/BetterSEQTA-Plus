724b6c2f6bcf365d316e842961313468fa923d1b
sethburkart123 <seth.burkart@hills.adventist.edu.au>
2024-11-01 16:37:30 +1100

    fix(Popup): hide theme settings on external popup (as variables can't be accessed from there)

--- src/svelte-interface/pages/settings/theme.svelte
+++ src/svelte-interface/pages/settings/theme.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import BackgroundSelector from "@/svelte-interface/components/themes/BackgroundSelector.svelte"
   import ThemeSelector from "@/svelte-interface/components/themes/ThemeSelector.svelte"
+  import { standalone } from "@/svelte-interface/utils/standalone.svelte"
   
   // backgrounds
   let selectedBackground = $state<string | null>(null);
@@ -11,17 +12,25 @@
 </script>
 
 <div class="py-4">
-  <button
-    onclick={() => selectNoBackground()}
-    class="w-full px-4 py-2 mb-4 text-[13px] dark:text-white transition rounded-xl bg-zinc-200 dark:bg-zinc-700/50">
-    { clearTheme ? 'Clear Theme' : 'Select a Theme' }
-  </button>
-  <div class="relative w-full">
+  {#if !standalone}
     <button
-      onclick={() => editMode = !editMode}
-      class="absolute top-0 right-0 z-10 w-8 h-8 text-lg rounded-xl font-IconFamily bg-zinc-100 dark:bg-zinc-700">{editMode ? '\ue9e4' : '\uec38'}</button>
+      onclick={() => selectNoBackground()}
+      class="w-full px-4 py-2 mb-4 text-[13px] dark:text-white transition rounded-xl bg-zinc-200 dark:bg-zinc-700/50">
+      { clearTheme ? 'Clear Theme' : 'Select a Theme' }
+    </button>
+    <div class="relative w-full">
+      <button
+        onclick={() => editMode = !editMode}
+        class="absolute top-0 right-0 z-10 w-8 h-8 text-lg rounded-xl font-IconFamily bg-zinc-100 dark:bg-zinc-700">{editMode ? '\ue9e4' : '\uec38'}</button>
 
-    <BackgroundSelector isEditMode={editMode} bind:selectedBackground={selectedBackground} bind:selectNoBackground={selectNoBackground} />
-    <ThemeSelector isEditMode={editMode} />
-  </div>
+      <BackgroundSelector isEditMode={editMode} bind:selectedBackground={selectedBackground} bind:selectNoBackground={selectNoBackground} />
+      <ThemeSelector isEditMode={editMode} />
+    </div>
+  {:else}
+    <div class="flex items-center justify-center w-full h-full">
+      <div class="text-lg">
+        Open SEQTA and use the embedded settings to access theme settings. 🫠
+      </div>
+    </div>
+  {/if}
 </div>
