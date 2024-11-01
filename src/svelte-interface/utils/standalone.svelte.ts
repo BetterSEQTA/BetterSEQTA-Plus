6b6ac560f429991e32ba27c9ec6204be14b2fabe
sethburkart123 <seth.burkart@hills.adventist.edu.au>
2024-11-01 16:31:23 +1100

    fix: standalone not correctly being set

--- src/svelte-interface/utils/standalone.svelte.ts
+++ src/svelte-interface/utils/standalone.svelte.ts
@@ -1,16 +1,36 @@
-export function createStandalone() {
-	let standalone = $state(false);
+import type { Subscriber, Unsubscriber } from "svelte/store";
 
-	function setStandalone(value: boolean) {
-		standalone = value;
+export class Standalone {
+	private static instance: Standalone;
+	private _standalone = $state(false);
+	private subscribers = new Set<Subscriber<boolean>>();
+
+	private constructor() {}
+
+	public static getInstance(): Standalone {
+		if (!Standalone.instance) {
+			Standalone.instance = new Standalone();
+		}
+		return Standalone.instance;
 	}
 
-	return {
-		get standalone() {
-			return standalone;
-		},
-		setStandalone
-	};
+	public setStandalone(value: boolean) {
+		this._standalone = value;
+		this.subscribers.forEach(subscriber => subscriber(value));
+	}
+
+	public get standalone() {
+		return this._standalone;
+	}
+
+	public subscribe(run: Subscriber<boolean>): Unsubscriber {
+		this.subscribers.add(run);
+		run(this._standalone);
+
+		return () => {
+			this.subscribers.delete(run);
+		};
+	}
 }
 
-export const standalone = createStandalone();
+export const standalone = Standalone.getInstance();
