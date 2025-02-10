1fbd9a6c1b78f762a76d7efc84097543fad644a0
Alphons Joseph <93847055+Crazypersonalph@users.noreply.github.com>
2025-02-10 19:49:43 +0800

    FR #203

--- src/interface/components/ColourPicker.tsx
+++ src/interface/components/ColourPicker.tsx
@@ -93,7 +93,7 @@ export default function Picker({
     <ColorPicker
       disableDarkMode={true}
       presets={presets}
-      hideInputs={true}
+      hideInputs={false}
       value={customThemeColor ?? ""}
       onChange={(color: string) => {
         if (customOnChange) {
