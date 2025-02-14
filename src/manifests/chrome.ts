f7dc8013ecc6b8aae3cc266acd103a345de09349
SethBurkart123 <seth.burkart@hills.adventist.edu.au>
2025-02-14 17:11:33 +1100

    feat: rely on package.json for version and description

--- src/manifests/chrome.ts
+++ src/manifests/chrome.ts
@@ -1,4 +1,9 @@
 import { createManifest } from '../../lib/createManifest'
 import baseManifest from './manifest.json'
+import pkg from '../../package.json'
 
-export const chrome = createManifest(baseManifest, 'chrome')
+export const chrome = createManifest({
+    ...baseManifest,
+    version: pkg.version,
+    description: pkg.description,
+}, 'chrome')
--- src/manifests/opera.ts
+++ src/manifests/opera.ts
@@ -1,4 +1,9 @@
 import { createManifest } from '../../lib/createManifest'
 import baseManifest from './manifest.json'
+import pkg from '../../package.json'
 
-export const opera = createManifest(baseManifest, 'opera')
+export const opera = createManifest({
+    ...baseManifest,
+    version: pkg.version,
+    description: pkg.description,
+}, 'opera')
