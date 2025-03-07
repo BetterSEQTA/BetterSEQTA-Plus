ec0d3e676d94ecbf62e1e5e7cae74dc2852c547d
Andrew R <loafing@sockycat.net>
2025-03-07 13:30:30 +1030

    Update SEQTA.ts

--- src/SEQTA.ts
+++ src/SEQTA.ts
@@ -3938,7 +3938,7 @@ async function handleDirectMessages(node: Element): Promise<void> { // Only a bi
           ef.remove()
           var id = fields.indexOf(id)
           fields.splice(id, 1)    
-        });
+        };
         e.append(deletebutton)	    
       }
     };
