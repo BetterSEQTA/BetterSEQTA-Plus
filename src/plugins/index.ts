0de1cebeca332e1848aa656b044cfbae13e1639d
SethBurkart123 <seth.burkart@hills.adventist.edu.au>
2025-03-21 17:59:28 +1100

    chore: hide test plugin

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -3,7 +3,6 @@ import { PluginManager } from './core/manager';
 // plugins
 import timetablePlugin from './built-in/timetable';
 import notificationCollectorPlugin from './built-in/notificationCollector';
-import testPlugin from './built-in/test';
 
 // Initialize plugin manager
 const pluginManager = PluginManager.getInstance();
@@ -11,7 +10,7 @@ const pluginManager = PluginManager.getInstance();
 // Register built-in plugins
 pluginManager.registerPlugin(timetablePlugin);
 pluginManager.registerPlugin(notificationCollectorPlugin);
-pluginManager.registerPlugin(testPlugin);
+//pluginManager.registerPlugin(testPlugin);
 
 // Legacy plugin exports
 export { init as Monofile } from './monofile';
