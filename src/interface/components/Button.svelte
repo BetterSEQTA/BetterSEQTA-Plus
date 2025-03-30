72de8ec6c03d71efcac5482a6f793616ce58d5ba
SethBurkart123 <seth.burkart@hills.adventist.edu.au>
2025-03-30 12:33:28 +1100

    style: improve button

--- src/interface/components/Button.svelte
+++ src/interface/components/Button.svelte
@@ -2,6 +2,6 @@
   let { onClick, text } = $props<{ onClick: () => void, text: string, [key: string]: any }>();
 </script>
 
-<button onclick={onClick} class='px-14 py-1 text-[0.75rem] dark:bg-[#38373D] bg-[#DDDDDD] dark:text-white rounded-md'>
+<button onclick={onClick} class='px-4 py-1 text-[0.75rem] dark:bg-[#38373D] bg-[#DDDDDD] dark:text-white rounded-md'>
   {text}
 </button>
