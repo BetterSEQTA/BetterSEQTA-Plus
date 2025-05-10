756adddd63fbccf9b9f4a6e81944cbaefd5d5dba
StroepWafel <109832156+NIDNHU@users.noreply.github.com>
2025-05-10 15:13:20 +0930

    Update postcss.config.cjs

--- src/postcss.config.cjs
+++ src/postcss.config.cjs
@@ -1,6 +1,11 @@
+// This is the PostCSS configuration file for your project
+
 module.exports = {
   plugins: {
+    // TailwindCSS plugin to enable Tailwind CSS functionality
     tailwindcss: {},
+
+    // Autoprefixer plugin to automatically add vendor prefixes to CSS for cross-browser compatibility
     autoprefixer: {},
   },
 };
