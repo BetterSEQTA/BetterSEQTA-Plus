ea356a0a17f6ecd5ebd2a60b19308e714a5016cc
StroepWafel <109832156+NIDNHU@users.noreply.github.com>
2025-05-11 09:34:45 +0930

    Update delay.ts - comment

--- src/seqta/utils/delay.ts
+++ src/seqta/utils/delay.ts
@@ -1,3 +1,4 @@
+// Function to create a delay using a promise
 export function delay(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms)); // Resolves after the specified number of milliseconds
 }
