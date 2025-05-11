cd14b9ab22a069658d75699f3e12b7d694444092
StroepWafel <109832156+NIDNHU@users.noreply.github.com>
2025-05-11 09:34:29 +0930

    Update debounce.ts - comment

--- src/seqta/utils/debounce.ts
+++ src/seqta/utils/debounce.ts
@@ -1,10 +1,15 @@
+// Function to create a debounced version of a function
 export default function debounce<T extends (...args: any[]) => void>(
-  fn: T,
-  delay: number,
-): (...args: Parameters<T>) => void {
-  let timeout: ReturnType<typeof setTimeout>;
+  fn: T, // The function to be debounced
+  delay: number, // The delay (in milliseconds) after which the function will be called
+): (...args: Parameters<T>) => void { // Returns a debounced version of the function
+  let timeout: ReturnType<typeof setTimeout>; // Variable to store the timeout ID
+
   return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
+    // Clears the previous timeout each time the function is called
     clearTimeout(timeout);
+
+    // Sets a new timeout to call the function after the specified delay
     timeout = setTimeout(() => fn.apply(this, args), delay);
   };
 }
