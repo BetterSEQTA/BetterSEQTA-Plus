50de668d0154aa5251de2bc02fdb7c01e46f2b5f
SethBurkart123 <seth.burkart@hills.adventist.edu.au>
2026-03-18 09:52:30 +1100

    style: add iconfamily icon to cloud signin

--- src/interface/components/store/CloudHeader.svelte
+++ src/interface/components/store/CloudHeader.svelte
@@ -93,9 +93,7 @@
         {cloudState.user?.displayName || cloudState.user?.username || cloudState.user?.email || "User"}
       </span>
     {:else}
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 15a4.5 4.5 0 004.5 4.5H18a3.75 3.75 0 001.332-7.257 3 3 0 00-3.758-3.848 5.25 5.25 0 00-10.233 2.33A4.502 4.502 0 0012.75 15h-10.5z" />
-      </svg>
+      <span class="text-xl font-IconFamily" aria-hidden="true">{'\ued53'}</span>
       <span class="text-base font-medium">Sign in</span>
     {/if}
   </button>
