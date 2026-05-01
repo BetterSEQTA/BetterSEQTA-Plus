608fc96c4eb664616bfc8489cbea660fbcebad86
SethBurkart123 <seth.burkart@hills.adventist.edu.au>
2026-05-01 15:39:20 +1000

    chore: temporarily disable message folders plugin and remove from changelog

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -10,7 +10,7 @@ import assessmentsAveragePlugin from "./built-in/assessmentsAverage";
 import profilePicturePlugin from "./built-in/profilePicture";
 import assessmentsOverviewPlugin from "./built-in/assessmentsOverview";
 import backgroundMusicPlugin from "./built-in/backgroundMusic";
-import messageFoldersPlugin from "./built-in/messageFolders";
+//import messageFoldersPlugin from "./built-in/messageFolders";
 //import testPlugin from './built-in/test';
 
 // Heavy plugins (lazy-loaded only when enabled)
@@ -29,7 +29,7 @@ pluginManager.registerPlugin(timetableEditPlugin);
 pluginManager.registerPlugin(profilePicturePlugin);
 pluginManager.registerPlugin(assessmentsOverviewPlugin);
 pluginManager.registerPlugin(backgroundMusicPlugin);
-pluginManager.registerPlugin(messageFoldersPlugin);
+//pluginManager.registerPlugin(messageFoldersPlugin);
 //pluginManager.registerPlugin(testPlugin);
 
 // Register heavy plugins with lazy loading
